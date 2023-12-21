b52fdd49dd11a7fc3d35e168a6f2dd8105852475
devilGhost <kumrahul1999@gmail.com>
2023-12-21 12:48:18 +0530

    ang-data&eventBinding

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet, RouterLink, FormsModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  username = '';
+  password = '';
 
+  handleOnclick() {
+    this.username = '';
+  }
+
+  handleInput(event:any){
+    this.password = event.target.value
+  }
 }
